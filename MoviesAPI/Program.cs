bc85feed008c614ff389f8567254e8af6db9a3f6
AfnanElhussini <afnanrlhussini23@gmail.com>
2023-05-13 21:33:04 +0300

    Add Options to Swagger

--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 namespace MoviesAPI
 {
     public class Program
@@ -11,7 +14,22 @@ namespace MoviesAPI
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(name: "V1", info: new OpenApiInfo
+                {
+                    Version = "V1",
+                    Title = "Movies API TEST",
+                    Description = "A Movies API is a web service that allows developers to access a database of movie information",
+                    TermsOfService = new Uri(uriString: "https://www.google.com"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Afnan",
+                        Email = "afnan@domain.com"
+                    },
+                   
+                });
+            });
 
             var app = builder.Build();
 
@@ -19,7 +37,10 @@ namespace MoviesAPI
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/V1/swagger.json", "Movies API V1");
+                });
             }
 
             app.UseHttpsRedirection();
